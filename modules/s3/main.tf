0aeec293bfaf3c7cae5c126b81385999f6e19913
Saurav Aryal <aryalsaurav01@gmail.com>
2026-08-20 21:34:22 +0545

    fix: s3 public access and static files access

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -18,10 +18,30 @@ resource "aws_s3_bucket_ownership_controls" "storage_bucket" {
 }
 
 
-resource "aws_s3_bucket_acl" "storage_bucket" {
-  depends_on = [aws_s3_bucket_ownership_controls.storage_bucket]
-
+resource "aws_s3_bucket_public_access_block" "storage_bucket" {
   bucket = aws_s3_bucket.storage_bucket.id
-  acl    = "private"
 
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "storage_bucket" {
+  bucket = aws_s3_bucket.storage_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadStatic"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.storage_bucket.arn}/static/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.storage_bucket]
 }
